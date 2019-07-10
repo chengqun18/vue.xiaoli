fa058ba517f8c40c7ac7e47560ecb3a7458d2efc
chengqun18 <15221023624@163.com>
2019-07-10 16:25:37 +0800

    第一次项目开始的时候--vue

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>MemberContainer</h3>
+    <mt-button type="primary">primary</mt-button>
+    <button type="button" class="mui-btn mui-btn-warning">黄色</button>
+  </div>
+</template>
+
+<script>
+  import { Toast } from 'mint-ui';
+  
+  export default {
+    created() {
+      Toast({
+        // message: '小姐姐真好看',
+        duration: 3000
+      });
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
